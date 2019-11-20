ce04b969f9b028013578fbfd4a725a0bdf95bcc6
unknown <atgree1@ilstu.edu>
2019-11-20 17:53:38 -0600

    added my timer

---
+++ AlarmAndroid/app/realapp/src/main/java/it226/alarm/location_page.java
@@ -0,0 +1,14 @@
+package com.example.alarmbuttonstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class location_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_page);
+    }
+}
