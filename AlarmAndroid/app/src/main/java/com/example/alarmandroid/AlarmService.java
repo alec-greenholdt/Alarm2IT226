fafde7c5c329bd34c3b52bd052d81cddf4d4db2a
Kalka, Kasey <kmkalka@ilstu.edu>
2019-11-21 14:10:17 -0600

    test for alec

--- AlarmAndroid/app/src/main/java/com/example/alarmandroid/AlarmService.java
+++ AlarmAndroid/app/src/main/java/com/example/alarmandroid/AlarmService.java
@@ -29,7 +29,7 @@ public class AlarmService extends IntentService {
                 new Intent(this, OneTimeActivity.class), 0);
 
         NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
-                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher_background)
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher)
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                 .setContentText(msg);
 
