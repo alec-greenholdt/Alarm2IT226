23e9829864f045f9ef5ace022d7aa01a42636808
Gomez, Kevin <krgomez@ilstu.edu>
2019-11-21 12:48:35 -0600

    Added UI

---
+++ AlarmAndroid/app/src/main/java/com/example/alarmandroid/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.alarmandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.AlarmButton);
+        Button b2=(Button)findViewById(R.id.timerButton);
+        Button b3= (Button) findViewById(R.id.locationButton);
+    }
+
+    public void onClick(View v){
+        Intent alarm = new Intent(MainActivity.this, alarmPage.class);
+        startActivity(alarm);
+
+    }
+
+    public void timeClick(View v){
+        Intent timer = new Intent(MainActivity.this, timer_page.class);
+        startActivity(timer);
+    }
+
+    public void locationClick (View v){
+        Intent location = new Intent(MainActivity.this, location_page.class);
+        startActivity(location);
+    }
+
+
+}
---
+++ AlarmAndroid/app/src/main/java/com/example/alarmandroid/timer_page.java
@@ -0,0 +1,13 @@
+package com.example.alarmandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class timer_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_page);
+    }
+}
