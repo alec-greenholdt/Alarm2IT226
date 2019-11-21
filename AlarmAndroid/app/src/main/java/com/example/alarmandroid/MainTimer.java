b1af23061582e98df522c49c063daace9381e903
unknown <atgree1@ilstu.edu>
2019-11-20 21:42:53 -0600

    added my own UI

--- AlarmAndroid/app/src/main/java/com/example/alarmandroid/MainTimer.java
+++ AlarmAndroid/app/src/main/java/com/example/alarmandroid/MainTimer.java
@@ -25,7 +25,7 @@ public class MainTimer extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.tab1_fragment);
 
             mTextViewCountDown = findViewById(R.id.text_view_countdown);
 
