5cd4234e93103a4bb3310d066ba69f61a9537e5b
unknown <atgree1@ilstu.edu>
2019-12-04 16:21:45 -0600

    wg

---
+++ working/app/src/main/java/com/example/working/Alarm.java
@@ -0,0 +1,4 @@
+package com.example.working;
+
+public class Alarm {
+}
--- working/app/src/main/java/com/example/working/MainActivity.java
+++ working/app/src/main/java/com/example/working/MainActivity.java
@@ -4,11 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity {
+    private Button b1;
+    private Button b2;
+    private Button b3;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         b1=(Button)findViewById(R.id.alarmButton);
+         b2=(Button)findViewById(R.id.timerButton);
+         b3= (Button) findViewById(R.id.locationButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent alarm = new Intent(MainActivity.this, Alarm.class);
+                startActivity(alarm);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent timer = new Intent(MainActivity.this, TimerTime.class);
+                startActivity(timer);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent location = new Intent(MainActivity.this, Location.class);
+                startActivity(location);
+            }
+        });
+
     }
+
+
+    public void onClick(View v){
+        Intent alarm = new Intent(MainActivity.this, Alarm.class);
+        startActivity(alarm);
+
+    }
+
+    public void timeClick(View v){
+        Intent timer = new Intent(MainActivity.this, TimerTime.class);
+        startActivity(timer);
+    }
+
+    public void locationClick (View v){
+        Intent location = new Intent(MainActivity.this, Location.class);
+        startActivity(location);
+    }
+
+
 }
+
